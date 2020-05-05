9fe0ba50c17237d02d5eed68971671f9cce36ae1
ShadowTinker <shadowtinkery@gmail.com>
2020-05-05 21:30:43 +0800

    Initialization

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Homepage from '../views/Homepage.vue';
+import GameInterface from '../views/GameInterface.vue';
+import MapEditor from '../views/MapEditor.vue';
 
 Vue.use(VueRouter);
 
@@ -8,15 +10,17 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Homepage,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/GameInterface',
+    name: '游戏界面',
+    component: GameInterface,
+  },
+  {
+    path: '/MapEditor',
+    name: '地图编辑器',
+    component: MapEditor,
   },
 ];
 
---
+++ src/views/MapEditor.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>MapEditor</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
