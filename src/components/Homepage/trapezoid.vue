96e57de5811b817f6e4d8c5b81f25b268a0ef727
ShadowTinker <shadowtinkery@gmail.com>
2020-05-05 21:42:24 +0800

    add a trapezoid.vue file for header in the top of the website

---
+++ src/components/Homepage/trapezoid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{content}}</div>
+</template>
+
+<script>
+export default {
+  name: 'trapezoid',
+  data() {
+    return {
+      content: 'NULL',
+    };
+  },
+  methods: {
+    changeContent(newContent) {
+      this.content = newContent;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
