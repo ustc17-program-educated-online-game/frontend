99fa9f563decc64c0e7755c8f79f583abde2ae13
THY <iamthy@mail.ustc.edu.cn>
2020-06-04 10:21:38 +0800

    完成根据用户状态显示导航栏
    当用户处于登录状态显示账号管理与欢迎语句，处于未登录状态显示登录按钮

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,8 +1,5 @@
 <template>
     <div>
-        <div v-show = "DataSet.state">
-            欢迎
-        </div>
 
         <b-navbar toggleable="lg" type="dark" variant="info">
             <b-navbar-brand href="/">编程教育游戏</b-navbar-brand>
@@ -20,20 +17,27 @@
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
                     <b-nav-form>
-                    <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                    <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
                     </b-nav-form>
-
+                    <div v-show = "DataSet.state === 1">
+                        <b-nav-item>欢迎玩家{{DataSet.username}}</b-nav-item>
+                    </div>
                     <!-- <div>欢迎</div> -->
                     <b-nav-item href="/#/">注册新账号</b-nav-item>
-                    <b-nav-item-dropdown right>
-                    <!-- Using 'button-content' slot -->
-                    <template v-slot:button-content>
-                        我的账号
-                    </template>
-                    <b-dropdown-item href="#">账号信息</b-dropdown-item>
-                    <b-dropdown-item href="#">退出登录</b-dropdown-item>
-                    </b-nav-item-dropdown>
+                    <div v-show = "DataSet.state === 0">
+                        <b-nav-item href="/#/">登录</b-nav-item>
+                    </div>
+                    <div v-show = "DataSet.state === 1">
+                        <b-nav-item-dropdown right>
+                        <!-- Using 'button-content' slot -->
+                            <template v-slot:button-content>
+                                我的账号
+                            </template>
+                            <b-dropdown-item href="#">账号信息</b-dropdown-item>
+                            <b-dropdown-item href="#">退出登录</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                    </div>
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
