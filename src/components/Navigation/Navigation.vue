6c8fc7c52742a9ec22eb4579a4a4e799e57e310d
THY <iamthy@mail.ustc.edu.cn>
2020-06-05 16:58:23 +0800

    完成了关卡选择页面
    还要填链接，加图片

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -10,6 +10,7 @@
                 <b-navbar-nav>
                     <b-nav-item href="/">首页</b-nav-item>
                     <b-nav-item href="/#/GameInterface">开始游戏</b-nav-item>
+                    <b-nav-item href="/#/StageSelect">关卡选择</b-nav-item>
                     <b-nav-item href="/#/MapEditor">地图编辑器</b-nav-item>
                     <b-nav-item href="/#/">玩家社区</b-nav-item>
                 </b-navbar-nav>
---
+++ src/views/StageSelect.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <stage></stage>
+  </div>
+  <!---->
+</template>
+
+<script>
+// @ is an alias to /src
+import Stage from '../components/StageSelect/Stage.vue';
+
+export default {
+  name: 'StageSelect',
+  components: {
+    Stage,
+  },
+};
+</script>
